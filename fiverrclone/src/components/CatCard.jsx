eddfba578bed17eb49d9d93bc493642d77bb22e9
adexfit <y.adeboye@gmail.com>
2025-10-05 18:03:34 +0100

    I added all the basic placeholder for the needed components

---
+++ fiverrclone/src/components/CatCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CatCard = () => {
+  return <div>catCard</div>;
+};
+
+export default CatCard;
---
+++ fiverrclone/src/components/TrustedBy.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrustedBy = () => {
+  return <div>TrustedBy</div>;
+};
+
+export default TrustedBy;
