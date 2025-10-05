0cee0e43946b4cf9cb5fd9f5c371a278feba955c
adexfit <y.adeboye@gmail.com>
2025-10-05 18:18:52 +0100

    I styled the catCard

--- fiverrclone/src/components/CatCard.jsx
+++ fiverrclone/src/components/CatCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const CatCard = () => {
-  return <div>catCard</div>;
-};
+function CatCard({ card }) {
+  return (
+    <Link to="/gigs?cat=design">
+      <div className="w-[252px] h-[344px] text-white rounded relative cursor-pointer">
+        <img
+          src={card.img}
+          alt=""
+          className="w-full h-full object-cover rounded"
+        />
+        <span className="font-light absolute top-[15px] left-[15px]">
+          {card.desc}
+        </span>
+        <span className="absolute top-[40px] left-[15px] text-2xl font-medium">
+          {card.title}
+        </span>
+      </div>
+    </Link>
+  );
+}
 
 export default CatCard;
