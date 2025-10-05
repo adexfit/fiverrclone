303b7dccef2eb6ef203d387ed4a3c6ece07fc899
adexfit <y.adeboye@gmail.com>
2025-10-05 18:43:39 +0100

    I styled the projectcard component

--- fiverrclone/src/components/ProjectCard.jsx
+++ fiverrclone/src/components/ProjectCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
-};
+function ProjectCard({ card }) {
+  return (
+    <div className="w-[300px] h-[300px] rounded-md overflow-hidden cursor-pointer border border-[#e1e1e1]">
+      <img src={card.img} alt="" className="w-full h-[70%] object-cover" />
+      <div className="flex items-center gap-5 p-4">
+        <img
+          src={card.pp}
+          alt=""
+          className="w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h2 className="text-sm font-medium">{card.cat}</h2>
+          <span className="text-sm font-light">{card.username}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default ProjectCard;
