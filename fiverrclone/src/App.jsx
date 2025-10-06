360ac76a4ff5edee631c3ffa6b5c34abba099276
adexfit <y.adeboye@gmail.com>
2025-10-06 15:47:03 +0100

    I added a dummy data component

--- fiverrclone/src/App.jsx
+++ fiverrclone/src/App.jsx
@@ -1,16 +1,80 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./app.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Gigs from "./pages/Gigs";
+import Gig from "./pages/Gig";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Add from "./pages/Add";
+import Orders from "./pages/Orders";
+import Messages from "./pages/Messages";
+import Message from "./pages/Message";
+import MyGigs from "./pages/MyGigs";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
 
-  return (
-    <>
-      <div className="text-red-700">Hello world</div>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
